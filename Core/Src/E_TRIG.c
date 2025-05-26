6a1e913dc97d0509225ed9007c37cfd8d4d75bbd
SriramCodeOnChip <ssri0091@gmail.com>
2025-05-26 15:16:18 +0530

    Added initial code for edge trigger

--- Core/Src/E_TRIG.c
+++ Core/Src/E_TRIG.c
@@ -1,1 +1,15 @@
+/*
+ * E_TRIG.c
+ *
+ *  Created on: May 23, 2020
+ *      Author: Sriram
+ */
 
+#include <E_TRIG.h>
+
+void E_TRIG_Update(E_TRIG *trig, bool clk)
+{
+	trig->Q = clk && !(trig->Stat_Bit);
+	trig->FT_Q = trig->Stat_Bit && !clk;
+	trig->Stat_Bit = clk;
+}
